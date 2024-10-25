44bc78e85309049b140ea050c4e13af67fe32d57
Agustín Longoni <agustinlongoni@gmail.com>
2024-10-25 17:00:59 -0300

    fix: replace Zeebu aToken

--- src/modules/reserve-overview/TokenLinkDropdown.tsx
+++ src/modules/reserve-overview/TokenLinkDropdown.tsx
@@ -117,7 +117,7 @@ export const TokenLinkDropdown = ({
           <Box>
             <Box sx={{ px: 4, pt: 3, pb: 2 }}>
               <Typography variant="secondary12" color="text.secondary">
-                <Trans>Aave aToken</Trans>
+                <Trans>Zeebu aToken</Trans>
               </Typography>
             </Box>
 
