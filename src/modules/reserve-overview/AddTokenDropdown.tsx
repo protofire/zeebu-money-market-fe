02f0684a19700bdcf6c857402532fe816c0059eb
Luis Lopez <luis.lopez@protofire.io>
2024-10-28 13:54:59 -0300

    aToken -> zToken (#25)

--- src/modules/reserve-overview/AddTokenDropdown.tsx
+++ src/modules/reserve-overview/AddTokenDropdown.tsx
@@ -163,7 +163,7 @@ export const AddTokenDropdown = ({
           <Box>
             <Box sx={{ px: 4, pt: 3, pb: 2 }}>
               <Typography variant="secondary12" color="text.secondary">
-                <Trans>Zeebu aToken</Trans>
+                <Trans>Zeebu zToken</Trans>
               </Typography>
             </Box>
             <MenuItem
