c2ab8dfcd8f2537c8445a8d8bcd6b655c8db2b49
Agustín Longoni <agustinlongoni@gmail.com>
2024-10-29 15:17:38 -0300

    fix: wallet improvements on styles

--- src/components/transactions/FlowCommons/TxModalTitle.tsx
+++ src/components/transactions/FlowCommons/TxModalTitle.tsx
@@ -8,7 +8,11 @@ export type TxModalTitleProps = {
 
 export const TxModalTitle = ({ title, symbol }: TxModalTitleProps) => {
   return (
-    <Typography variant="h2" color="white" sx={{ mb: 6 }}>
+    <Typography
+      variant="h3"
+      color="white"
+      sx={{ p: 4, borderBottom: '1px solid rgba(255, 255, 255, 0.3)', flex: '0 0 auto' }}
+    >
       {title} {symbol ?? ''}
     </Typography>
   );
