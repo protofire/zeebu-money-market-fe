a5fa86d49a492a3a2e71a27bf0d23c1e6cd7f3bb
Agustín Longoni <agustinlongoni@gmail.com>
2024-11-22 12:35:52 -0300

    fix: tooltip text and add gradien indicator and popover component

--- src/components/transactions/Borrow/CDRSlider.tsx
+++ src/components/transactions/Borrow/CDRSlider.tsx
@@ -1,11 +1,12 @@
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import MuiInput from '@mui/material/Input';
+import Popover from '@mui/material/Popover';
 import Slider, { SliderProps } from '@mui/material/Slider';
 import { styled } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
-import * as React from 'react';
-import { CollateralRatioTooltip } from 'src/components/infoTooltips/CollateralRatioTooltip';
+import React, { useState } from 'react';
+import { LTVTooltip } from 'src/components/infoTooltips/LTVTooltip';
 
 const Input = styled(MuiInput)(() => ({
   width: '70px',
@@ -27,14 +28,17 @@ const Input = styled(MuiInput)(() => ({
 
 const WhiteSlider = styled(Slider)<SliderProps>(() => ({
   color: '#FFFFFF',
+  margin: '8px 0 0 0',
   '& .MuiSlider-thumb': {
     backgroundColor: '#FFFFFF',
   },
   '& .MuiSlider-rail': {
     backgroundColor: '#FFFFFF',
+    height: '18px',
   },
   '& .MuiSlider-track': {
-    backgroundColor: '#FFFFFF',
+    height: '18px',
+    transition: 'background-image 0.3s ease',
   },
   '& .MuiSlider-valueLabel': {
     backgroundColor: '#FFFFFF',
@@ -57,6 +61,7 @@ type CDRSliderProps__Type = {
 
 export default function CDRSlider(props: CDRSliderProps__Type) {
   const { maxValue, minValue, value, onChange } = props;
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
 
   const midValue = (minValue + maxValue) / 2;
 
@@ -66,21 +71,51 @@ export default function CDRSlider(props: CDRSliderProps__Type) {
     { value: maxValue, label: 'High' },
   ];
 
-  const handleSliderChange = (_event: Event, newValue: number | number[]) => {
-    onChange(newValue as number);
+  const getTrackColor = () => {
+    if (value <= midValue) {
+      return value < (minValue + midValue) / 2
+        ? 'linear-gradient(231deg,#00c2a1,#ffef79)'
+        : 'linear-gradient(231deg,#ff895d,#ffcd4d)';
+    }
+    return 'linear-gradient(231deg,#d91838,#ff7881)';
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const handleSliderChange = (_event: Event, newValue: number | number[], _activeThumb: number) => {
+    const newValueNum = newValue as number;
+    onChange(newValueNum);
+
+    if (newValueNum > midValue) {
+      setAnchorEl(document.getElementById('input-slider') as HTMLElement); // Adjust anchor
+    } else {
+      setAnchorEl(null);
+    }
   };
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    onChange(event.target.value === '' ? minValue : Number(event.target.value));
+    const newValue = event.target.value === '' ? minValue : Number(event.target.value);
+    onChange(newValue);
+
+    if (newValue > midValue) {
+      setAnchorEl(event.currentTarget as HTMLElement);
+    } else {
+      setAnchorEl(null);
+    }
   };
 
+  const handleClosePopover = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+
   return (
-    <Box>
-      <Box sx={{ display: 'flex', flexDirection: 'row', margin: '2rem 0 0 0' }}>
+    <Box mb={4}>
+      <Box sx={{ display: 'flex', flexDirection: 'row', margin: '2rem 0 0' }}>
         <Typography id="input-slider" color="white">
-          Collateral Ratio
+          Loan to Value (LTV)
         </Typography>
-        <CollateralRatioTooltip />
+        <LTVTooltip />
       </Box>
       <Grid container spacing={8} sx={{ alignItems: 'center' }}>
         <Grid item xs>
@@ -92,6 +127,11 @@ export default function CDRSlider(props: CDRSliderProps__Type) {
             max={maxValue}
             min={minValue}
             marks={marks}
+            sx={{
+              '& .MuiSlider-track': {
+                backgroundImage: getTrackColor(),
+              },
+            }}
           />
         </Grid>
         <Grid item>
@@ -109,6 +149,36 @@ export default function CDRSlider(props: CDRSliderProps__Type) {
           />
         </Grid>
       </Grid>
+      <Popover
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClosePopover}
+        anchorOrigin={{
+          vertical: 'center',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        sx={{
+          '& .MuiPaper-root': {
+            backgroundColor: 'rgba(189, 31, 31, 0.26)', // Transparent white
+            boxShadow: 'none', // Remove shadow if needed
+            backdropFilter: 'blur(5px)', // Optional: Add blur for aesthetic effect
+            borderRadius: '6px',
+          },
+        }}
+      >
+        <Box sx={{ padding: '1rem', maxWidth: '200px' }}>
+          <Typography color="white" fontWeight="bold">
+            Warning: High Risk!
+          </Typography>
+          <Typography variant="main12">
+            You are entering a high-risk position. Please proceed with caution.
+          </Typography>
+        </Box>
+      </Popover>
     </Box>
   );
 }
