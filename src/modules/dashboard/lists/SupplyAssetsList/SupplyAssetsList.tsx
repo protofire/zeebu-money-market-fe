35d2b7338bad2ba99c30a7d10d3070ff5ffa0a36
Agustín Longoni <agustinlongoni@gmail.com>
2024-11-27 14:23:13 -0300

    fix: rm aave link

--- src/modules/dashboard/lists/SupplyAssetsList/SupplyAssetsList.tsx
+++ src/modules/dashboard/lists/SupplyAssetsList/SupplyAssetsList.tsx
@@ -16,7 +16,6 @@ import { fetchIconSymbolAndName } from 'src/ui-config/reservePatches';
 import { displayGhoForMintableMarket } from 'src/utils/ghoUtilities';
 
 import { ListWrapper } from '../../../../components/lists/ListWrapper';
-import { Link } from '../../../../components/primitives/Link';
 import {
   ComputedReserveData,
   useAppDataContext,
@@ -261,12 +260,7 @@ export const SupplyAssetsList = () => {
               <MarketWarning marketName="Ethereum AMM" />
             ) : user?.isInIsolationMode ? (
               <Warning severity="warning">
-                <Trans>
-                  Collateral usage is limited because of isolation mode.{' '}
-                  <Link href="https://docs.aave.com/faq/" target="_blank" rel="noopener">
-                    Learn More
-                  </Link>
-                </Trans>
+                <Trans>Collateral usage is limited because of isolation mode.</Trans>
               </Warning>
             ) : (
               filteredSupplyReserves.length === 0 &&
